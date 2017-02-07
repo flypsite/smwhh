87ed22641c2dfd033c219734a8e748b04a277c9c
Joachim Gola <joachim@viergleicheins.de>
2017-02-07 14:06:23 +0100

    fehler raus

--- src/comp/FrontPageLayout.js
+++ src/comp/FrontPageLayout.js
@@ -9,11 +9,9 @@ export default function FrontPageLayout(props) {
 	var w = 320;
 	var h = 800;
 	
-	function handleResize(e) {
-		console.log("handleResize", e);
-	}
+
 	return (
-		<div onResize={ handleResize }>
+		<div>
 			<FlypImage data={m.media} width={ w } height={ h }  />
 			<div className="Headline">{m.headline}</div>
 			<div className="Text">{m.text}</div>
