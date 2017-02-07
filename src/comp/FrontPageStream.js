cd00bf3f5a661572d0d35e281e5c93b39fd7b77e
Joachim Gola <joachim@viergleicheins.de>
2017-02-07 16:17:59 +0100

    dummytext

--- src/comp/FrontPageStream.js
+++ src/comp/FrontPageStream.js
@@ -65,6 +65,8 @@ class FrontPageStream extends Component {
 
     	return <div onClick={ () => self.clicked(item, artstr) }>
 	      <Message key={item.id} mode="frontpages" data={ item } />
+	      <div>        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+</div>
         {artstr}
 	    </div>
     });
