739c1f3818c1a366fe595895c6f6ffe2b47699b9
Matthias Oelmann <mao@viergleicheins.de>
2017-02-07 12:34:45 +0100

    FlypImage as Component

--- src/comp/FlypImage.js
+++ src/comp/FlypImage.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ImageMagic from '../util/ImageMagic.js';
 
-export default function FlypImage(props) {
-  
-	// the message
-	var media = props.data;
-	if ( ! media ) return <img/>;
-	var image = media.image;
-	if ( ! image ) return <img/>;
-	
-	var imgValues = {
-		'container_width': 320,
-		'container_height': 800,
-		'center_x': image.center?image.center.x:image.width/2,
-		'center_y': image.center?image.center.y:image.height/2,
-		'detail_x' : image.detail?image.detail.x:0,
-		'detail_y' : image.detail?image.detail.y:0,
-		'detail_width' : image.detail?image.detail.width:image.width,
-		'detail_height' : image.detail?image.detail.height:image.height, 
-		'zoom_mode' : image.zoom,
-		'image_width': image.width,
-		'image_height': image.height
-	};
 
-	var cssCalc = ImageMagic(imgValues);
-	cssCalc['background-image'] = 'url('+image.url+')';
+class FlypImage extends Component {
 
-	return (
-		<div className="Image" style={ cssCalc } />
-  );
-    
+	render() {  
+		// the message
+		var media = this.props.data;
+		if ( ! media ) return <img/>;
+		var image = media.image;
+		if ( ! image ) return <img/>;
+		
+		var imgValues = {
+			'container_width': 320,
+			'container_height': 800,
+			'center_x': image.center?image.center.x:image.width/2,
+			'center_y': image.center?image.center.y:image.height/2,
+			'detail_x' : image.detail?image.detail.x:0,
+			'detail_y' : image.detail?image.detail.y:0,
+			'detail_width' : image.detail?image.detail.width:image.width,
+			'detail_height' : image.detail?image.detail.height:image.height, 
+			'zoom_mode' : image.zoom,
+			'image_width': image.width,
+			'image_height': image.height
+		};
+
+		var cssCalc = ImageMagic(imgValues);
+		cssCalc['background-image'] = 'url('+image.url+')';
+
+		return (
+			<div className="Image" style={ cssCalc } />
+	  );
+	}    
 }
+
+export default FlypImage;
