492e12a9fb46198ca31ec2cd42bb3869155551e0
Matthias Oelmann <mao@viergleicheins.de>
2017-02-08 14:25:45 +0100

    stand der dinge

--- src/comp/ArticleStream.js
+++ src/comp/ArticleStream.js
@@ -3,60 +3,60 @@ import Message from './Message.js'
 
 class ArticleStream extends Component {
 
-  static contextTypes = { app: React.PropTypes.object }
+	static contextTypes = { app: React.PropTypes.object }
 
 
-  setStream(s) {
-      this.setState(s);
-  }
+	setStream(s) {
+		this.setState(s);
+	}
 
 
-  load(substream) {
+	load(substream) {
 
-    if ( this.loading ) return;
-    this.loading = true;
+		if ( this.loading ) return;
+		this.loading = true;
 
-    var self = this;
-    console.log('load ');
-    this.context.app.loadStreamFull(substream.key, function(stream) { 
-      self.setStream(stream);
-    });
-  }
-
-
-  render() {
-
-    if ( this.props.mustLoad ) {
-      this.load(this.props.data);
-    }
+		var self = this;
+		console.log('load ');
+		this.context.app.loadStreamFull(substream.key, function(stream) { 
+			self.setStream(stream);
+		});
+	}
 
 
-    var subinfo = this.props.data;
-    if ( ! subinfo ) {
-      return <div>no substream...</div>
-    }
+	render() {
+
+		if ( this.props.mustLoad ) {
+			this.load(this.props.data);
+		}
 
 
-    var stream = this.state;
-
-    if ( ! stream ) {
-      return <div>loading...</div>
-    }
-
-
-    const listItems = stream.items.map( (item) =>
-      <div key={item.id}>
-        <Message mode="article" data={ item }/>
-      </div>
-    );
+		var subinfo = this.props.data;
+		if ( ! subinfo ) {
+			return <div>no substream...</div>
+		}
 
 
-    return (
-      <div className="ArticleStream">
-          { listItems }
-      </div>
-    );
-  }
+		var stream = this.state;
+
+		if ( ! stream ) {
+			return <div>loading...</div>
+		}
+
+
+		const listItems = stream.items.map( (item) =>
+			<div key={item.id}>
+			<Message mode="article" data={ item }/>
+			</div>
+			);
+
+
+		return (
+			<div className="ArticleStream">
+			{ listItems }
+			</div>
+			);
+	}
 
 }
 
