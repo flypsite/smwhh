3f8dbc4ae60860ae13dde11e61981263328cb7b1
jo <joachim@viergleicheins.de>
2017-03-07 00:09:44 +0100

    some more Textmagic

--- src/util/TextMagic.js
+++ src/util/TextMagic.js
@@ -1,6 +1,8 @@
 export default function TextMagic(str) {
-	var result = str.replace(/\| */g, "\n")
-		.replace(/(http[a-zA-Z0-9\!\?\&\%\$\*\'\:\;\-\_\:\/\.]+)/g, "<a href='$1'>$1</a>");
+	var result = str
+		.replace(/\| */g, "\n")
+//		.replace(/\[Link: (.*)\]/g, "<a href='$1'>$1</a>");
+		.replace(/\[Link: (.*)\]/g, "$1");
 	//more to come
 	return result;
 }
