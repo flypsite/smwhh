af8f557492f2c3ba92617ec3269445fbf1f51995
Joachim Gola <joachim@viergleicheins.de>
2017-03-09 20:29:29 +0100

    cosmetics

--- src/util/TextMagic.js
+++ src/util/TextMagic.js
@@ -2,10 +2,6 @@ export default function TextMagic(str) {
 	var result = str
 		.replace(/\| */g, "\n")
 		.replace(/\[Link: (.*)\]/g, "<a href='$1'>$1</a>");
-//		.replace(/\[Link: (.*?)\]/g, "$1");
 
 	return {__html: result };
-
-	//more to come
-	//return result;
 }
