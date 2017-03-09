1c5b3560254b92025bc79eccabe4d8780445643c
jo <joachim@viergleicheins.de>
2017-03-09 09:50:36 +0100

    noch ein bisschen Sharing

--- src/comp/SharingIcons.js
+++ src/comp/SharingIcons.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function SharingIcons(props) {
 	var url = encodeURIComponent(document.location.href);
+	var hash = encodeURIComponent(props.data.replace(/#/g, ""));
 	var fburl = "https://www.facebook.com/sharer/sharer.php?u=" + url;
-	var twurl = "https://twitter.com/intent/tweet?hashtags=" + props.data.replace(/#/g, "") + "&url=" + url;
+	var twurl = "https://twitter.com/intent/tweet?hashtags=" + hash + "&url=" + url;
 	return (
 		<div className="SharingIcons">
 			<span className="hashtag">{props.data}</span>
